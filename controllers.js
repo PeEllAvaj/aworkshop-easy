387b8734a63dabd7e61bdbf902e07d3975a3b673
Stephen Fluin <stephen.fluin@gmail.com>
2015-03-21 04:30:42 -0500

    Initial Load of data

--- controllers.js
+++ controllers.js
@@ -1,5 +1,7 @@
 var app = angular.module('scheduleControllers', []);
 
-app.controller('MainCtrl', function($scope) {
-  $scope.testing = "Hello World";
+app.controller('MainCtrl', function($scope, $http) {
+  $http.get('http://mortalpowers.com/data.json').success(function(data) {
+    $scope.sessions = data["events"];
+  });
 });
